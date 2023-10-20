ae2dcb39063e9c498372f8c384d82818eef4b830
João Vitor Chaves dos Santos <124842918+JoaoSantos7@users.noreply.github.com>
2023-10-20 12:36:54 -0300

    Delete apps directory
    Marcelo havia duplicado

--- apps/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    titulo_post = models.CharField(max_length=100)
-    descricao_post = models.CharField(max_length=255)
-    data_post = models.DateTimeField('Data publicado')
-    img = models.ImageField(upload_to = 'uploads', default='')
-
